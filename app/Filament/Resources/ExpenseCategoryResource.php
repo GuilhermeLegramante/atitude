870ba5e1c81cb2fb25cf87ff2e2b56e5239f73d9
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-06-06 13:17:55 -0300

    .

--- app/Filament/Resources/ExpenseCategoryResource.php
+++ app/Filament/Resources/ExpenseCategoryResource.php
@@ -30,15 +30,15 @@ class ExpenseCategoryResource extends Resource
     public static function form(Form $form): Form
     {
         return $form->schema([
-            Forms\Components\TextInput::make('name')->required()->maxLength(255),
+            Forms\Components\TextInput::make('name')->label('Nome')->required()->maxLength(255),
         ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table->columns([
-            Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
-            Tables\Columns\TextColumn::make('created_at')->dateTime('d/m/Y H:i'),
+            Tables\Columns\TextColumn::make('name')->label('Nome')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('created_at')->label('Criado em')->dateTime('d/m/Y H:i'),
         ])->filters([])->actions([
             Tables\Actions\EditAction::make(),
             Tables\Actions\DeleteAction::make(),

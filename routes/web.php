894057fee4eef7ff6ea0dd8c956c5625c2faf2be
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-03-10 13:01:22 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -115,7 +115,7 @@ Route::get('/modulo/{class}/certificado', function (ClassModel $class) {
         'date'    => now()->format('d/m/Y')
     ];
 
-    $pdf = Pdf::loadView('certificates.course_completed', $data)
+    $pdf = Pdf::loadView('certificates.default', $data)
         ->setPaper('a4', 'landscape');
 
     return $pdf->stream("Certificado_{$class->name}.pdf");

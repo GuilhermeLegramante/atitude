fb36967329c6abcb48fec83a626bc9eee8f71eea
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-07-20 09:54:56 -0300

    .

--- resources/views/partials/whatsapp.blade.php
+++ resources/views/partials/whatsapp.blade.php
@@ -92,7 +92,7 @@
 
 
  <!-- 💬 Botão Flutuante WhatsApp -->
- <a href="https://wa.me/55991575856?text=Olá!%20Quero%20falar%20sobre%20a%20plataforma%20de%20idiomas." target="_blank"
+ <a href="https://wa.me/55991327539?text=Olá!%20Quero%20falar%20sobre%20a%20plataforma%20de%20idiomas." target="_blank"
      rel="noopener"
      class="fixed bottom-5 right-5 z-50 flex items-center justify-center 
           bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg 
